4903cbd92a9291302ee06b3f874d35eaa47a4fbc
David <ddosinski98@gmail.com>
2019-01-19 09:34:46 -0500

    Initial Testing of Knowledge Graph API, URL Parse functional.  - David

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
+import json
+import urllib.request
+import urllib.parse
+
 def main():
-    print("hello world")
-    print("David is testing this")
+    googleAPItest()
+
+def googleAPItest():
+    """Example of Python client calling Knowledge Graph Search API."""
+
+    api_key = '.AIzaSyB2yq7Wo-OpoA4ZOT60JcbK1B2cv9Lyg5E'
+    query = 'Taylor Swift'
+    service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
+    params = {
+        'query': query,
+        'limit': 10,
+        'indent': True,
+        'key': api_key,
+    }
+    url = service_url + '?' + urllib.parse.urlencode(params)
+    print (url)
+    # response = json.loads(urllib.request.urlopen(url).read())
+    # for element in response['itemListElement']:
+    #     print(element['result']['name'] + ' (' + str(element['resultScore']) + ')')
+
 
 main()
 
