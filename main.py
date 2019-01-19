8eaa2cf00813cf49331bdde447bfbae33c187ea0
David <ddosinski98@gmail.com>
2019-01-19 11:49:21 -0500

    Scrape the Name, Release Date, and Director  - David

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ def main():
 
 
 def beautifulSoupTest():
+    # Conversion from URL into Python Object Tree using urllib and BeautifulSoup
     service_url = 'https://www.google.com/search'
-    query = 'Good Will Hunting'
+    query = 'ShawShank Redemption'
     params = {
         'query': query,
     }
@@ -20,6 +21,15 @@ def beautifulSoupTest():
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     html = urllib.request.urlopen(req).read()
     soup = BeautifulSoup(html, 'html.parser')
+
+    results = soup.find_all(class_="FSP1Dd") # Movie Title
+    print(results[0].get_text())
+
+    results = soup.find_all(class_="A1t5ne")
+    print(results[0].get_text()) # Release Date
+    print(results[2].get_text()) # Directors
+
+    # "brYqc"
 
 main()
 
